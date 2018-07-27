1a320f3e7344b9ee53a62f1b18adcdf8e73f23fa
vinay1231 <41731317+vinay1231@users.noreply.github.com>
2018-07-27 14:25:18 +0530

    Create natural.java

---
+++ natural.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+			int n;
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		int a=(n*(n+1))/2;
+		{
+			System.out.println(a);
+		}
+	}
+}
